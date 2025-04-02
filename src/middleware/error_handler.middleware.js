72f8a6d1af8ec35255bc0334cb7e8b23191f07cb
rahulmp007 <rahulmelepurakkal14@gmail.com>
2025-04-02 19:27:53 +0530

    user create and user login done

--- src/middleware/error_handler.middleware.js
+++ src/middleware/error_handler.middleware.js
@@ -1,3 +1,8 @@
-const errorHandler = (err, req, res, next) => {};
+const errorHandler = (err, req, res, next) => {
+  console.error(`Error Handler :=> ${err} `);
+  const { message, statusCode } = err;
+
+  res.status(statusCode).json({ message: message });
+};
 
 module.exports = errorHandler;
--- src/repository/user.repository.js
+++ src/repository/user.repository.js
@@ -1,26 +1,57 @@
+const AppError = require("../error/app_error");
 const prisma = require("../prisma/prisma");
 const bcrypt = require("bcryptjs");
+const { generateToken } = require("../utils/token_util");
 class UserRepository {
   constructor(parameters) {}
 
   async addUser(userInfo) {
-    console.log("creating user");
-
     const { name, email, password } = userInfo;
-    console.log(name,email,password);
-    
-    const hashedPassword = await bcrypt.hash(password, 10);
+
     try {
-      const user = await prisma.users.create({
+      const isUserExists = await prisma.user.findUnique({ where: { email } });
+      console.log(` user : `, isUserExists);
+
+      if (isUserExists) {
+        throw new AppError("user already exists", 409);
+      }
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = await prisma.user.create({
         data: { name, email, password: hashedPassword },
       });
-      console.log(`user created : ${user}`);
     } catch (error) {
-      console.log(error);
+      throw error;
     } finally {
-        console.log(`finally`);
-        
       await prisma.$disconnect();
+    }
+  }
+
+  async loginUser(userInfo) {
+    try {
+      const { email, password } = userInfo;
+      const user = await prisma.user.findUnique({
+        where: { email },
+        // select: { id: true, name: true, email: true },
+      });
+
+      if (!user) {
+        throw new AppError("user not found", 404);
+      }
+
+      const isPasswordMatch = await bcrypt.compare(password, user.password);
+      if (!isPasswordMatch) {
+        throw new AppError("invalid password", 401);
+      }
+
+      const token = generateToken(user);
+
+      delete user.password;
+
+      return { ...user, token: token };
+    } catch (error) {
+      throw error;
+    } finally {
+      prisma.$disconnect();
     }
   }
 }
